40bb0f81a3bbb3f1beea1e42875f478a0ad294ac
Igor Sasaki <igor@softable.com.br>
2021-03-22 19:21:03 -0300

    implementado listagem de agendamentos

--- backend/src/routes/appointments.routes.ts
+++ backend/src/routes/appointments.routes.ts
@@ -4,8 +4,13 @@ import { startOfHour, parseISO, isEqual } from "date-fns";
 import AppointmentsRepository from "../repositories/AppointmentsRepository";
 
 const appointmentsRouter = Router();
-
 const appointmentsRepository = new AppointmentsRepository();
+
+appointmentsRouter.get("/", (request, response) => {
+  const appointments = appointmentsRepository.all()
+
+  return response.json(appointments)
+})
 
 appointmentsRouter.post("/", (request, response) => {
   const { provider, date } = request.body;
