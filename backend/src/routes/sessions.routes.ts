9b323f0f4e6daaceacc3220fa14dfe821e215812
Igor Sasaki <igor@softable.com.br>
2021-03-26 09:56:58 -0300

    Implementado rota de autenticação do usuário e token utilizando a biblioteca JsonWebToken para gerar um token no formato JWT

---
+++ backend/src/routes/sessions.routes.ts
@@ -0,0 +1,28 @@
+import { Router } from "express";
+import AuthenticateUserService from "../services/AuthenticateUserService";
+
+const sessionsRouter = Router();
+
+sessionsRouter.post("/", async (request, response) => {
+  try {
+    const { email, password } = request.body;
+
+    const authenticadeUser = new AuthenticateUserService();
+
+    const { user, token } = await authenticadeUser.execute({ email, password });
+
+    const userWithoutPassword = {
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      created_at: user.created_at,
+      updated_at: user.updated_at,
+    };
+
+    return response.json({ userWithoutPassword, token });
+  } catch (err) {
+    return response.status(400).json({ error: err.message });
+  }
+});
+
+export default sessionsRouter;
---
+++ backend/src/services/AuthenticateUserService.ts
@@ -0,0 +1,42 @@
+import { getRepository } from "typeorm";
+import { compare } from "bcryptjs";
+
+import User from "../models/User";
+import { sign } from "jsonwebtoken";
+
+interface Request {
+  email: string;
+  password: string;
+}
+
+interface Response {
+  user: User;
+  token: string
+}
+
+class AuthenticateUserService {
+  public async execute({ email, password }: Request): Promise<Response> {
+    const usersRepository = getRepository(User);
+
+    const user = await usersRepository.findOne({ where: { email } });
+
+    if (!user) {
+      throw new Error("Incorrect email/password combination");
+    }
+
+    const passwordMatched = await compare(password, user.password);
+
+    if (!passwordMatched) {
+      throw new Error("Incorrect email/password combination");
+    }
+
+    const token = sign({}, "25e09e6621695cba2583c2f1ae7dcc20", {
+      subject: user.id,
+      expiresIn: "1d",
+    });
+
+    return { user, token };
+  }
+}
+
+export default AuthenticateUserService;
