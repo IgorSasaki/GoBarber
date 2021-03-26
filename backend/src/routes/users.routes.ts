e111e7969b98fa6bcb994678567034ad10a11213
Igor Sasaki <igor@softable.com.br>
2021-03-26 09:36:42 -0300

    Implementado criptografia da senha utilizando a biblioteca bcryptjs

--- backend/src/routes/users.routes.ts
+++ backend/src/routes/users.routes.ts
@@ -16,7 +16,15 @@ usersRouter.post('/', async (request, response) => {
       password,
     });
 
-    return response.json(user);
+    const userWithoutPassword = {
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      created_at: user.created_at,
+      updated_at: user.updated_at,
+    };
+
+    return response.json(userWithoutPassword);
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
