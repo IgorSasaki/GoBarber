ec285cdf8c2efd0639d5a6c63085e8e7a132b8e4
Igor Sasaki <igor@softable.com.br>
2021-03-31 18:07:03 -0300

    Implementado service de trataiva de erros e impletentado-a globalmente

--- backend/src/middlewares/ensureAuthenticated.ts
+++ backend/src/middlewares/ensureAuthenticated.ts
@@ -2,6 +2,7 @@ import { NextFunction, Request, Response } from "express";
 import { verify } from "jsonwebtoken";
 
 import authConfig from "../config/auth";
+import AppError from "../errors/AppError";
 
 interface TokenPayload {
   iat: number;
@@ -17,7 +18,7 @@ export default function ensureAuthenticated(
   const authHeader = request.headers.authorization;
 
   if (!authHeader) {
-    throw new Error("JWT token is missing");
+    throw new AppError("JWT token is missing", 401);
   }
 
   const [, token] = authHeader.split(" ");
--- backend/src/routes/users.routes.ts
+++ backend/src/routes/users.routes.ts
@@ -5,21 +5,44 @@ import uploadConfig from "../config/upload";
 import ensureAuthenticated from "../middlewares/ensureAuthenticated";
 
 import CreateUserService from "../services/CreateUserService";
-import UpdateUserAvatarService from "../services/UpdateUserAvatarSrvice";
+import UpdateUserAvatarService from "../services/UpdateUserAvatarService";
 
 const usersRouter = Router();
 const upload = multer(uploadConfig);
 
 usersRouter.post("/", async (request, response) => {
-  try {
-    const { name, email, password } = request.body;
+  const { name, email, password } = request.body;
 
-    const createUser = new CreateUserService();
+  const createUser = new CreateUserService();
 
-    const user = await createUser.execute({
-      name,
-      email,
-      password,
+  const user = await createUser.execute({
+    name,
+    email,
+    password,
+  });
+
+  const userWithoutPassword = {
+    id: user.id,
+    name: user.name,
+    email: user.email,
+    avatar: user.avatar,
+    created_at: user.created_at,
+    updated_at: user.updated_at,
+  };
+
+  return response.json(userWithoutPassword);
+});
+
+usersRouter.patch(
+  "/avatar",
+  ensureAuthenticated,
+  upload.single("avatar"),
+  async (request, response) => {
+    const updateUserAvatar = new UpdateUserAvatarService();
+
+    const user = await updateUserAvatar.execute({
+      user_id: request.user.id,
+      avatarFileName: request.file.filename,
     });
 
     const userWithoutPassword = {
@@ -32,37 +55,6 @@ usersRouter.post("/", async (request, response) => {
     };
 
     return response.json(userWithoutPassword);
-  } catch (err) {
-    return response.status(400).json({ error: err.message });
-  }
-});
-
-usersRouter.patch(
-  "/avatar",
-  ensureAuthenticated,
-  upload.single("avatar"),
-  async (request, response) => {
-    try {
-      const updateUserAvatar = new UpdateUserAvatarService();
-
-      const user = await updateUserAvatar.execute({
-        user_id: request.user.id,
-        avatarFileName: request.file.filename,
-      });
-
-      const userWithoutPassword = {
-        id: user.id,
-        name: user.name,
-        email: user.email,
-        avatar: user.avatar,
-        created_at: user.created_at,
-        updated_at: user.updated_at,
-      };
-  
-      return response.json(userWithoutPassword);
-    } catch (err) {
-      return response.status(400).json({ error: err.message });
-    }
   }
 );
 
