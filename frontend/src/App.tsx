255070c59509b6ffb29044b74461c7a372a1627f
Igor Sasaki <igor@softable.com.br>
2021-04-07 21:46:11 -0300

    Criado estilização global e instalado biblioteca styled-components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,14 @@
-import React from 'react';
+// Bibliotecas Externa
+import React from "react";
+
+// Estilização
+import GlobalStyle from './styles/global'
 
 const App: React.FC = () => (
-  <h1>Hello World</h1>
-) 
+  <>
+    <h1>Hello World</h1>
+    <GlobalStyle />
+  </>
+);
 
 export default App;
