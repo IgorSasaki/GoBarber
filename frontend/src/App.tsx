80c2fdd97737cdc02af83fd5e60d09660bda24cb
Igor Sasaki <igor@softable.com.br>
2021-04-29 20:36:24 -0300

    Implementa função para logout da aplicação por meio do COntextAPI

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 // Componentes
 import SignIn from "./pages/SignIn";
-import {AuthProvider} from "./context/AuthContext";
+import {AuthProvider} from "./hooks/AuthContext";
 
 // Estilização
 import GlobalStyle from "./styles/global";
