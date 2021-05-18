b40b3cd4a3cb050ba444fc4d76569330a4648d8a
Igor Sasaki <igor@softable.com.br>
2021-05-17 22:36:42 -0300

    Alterado StatusBar

--- mobile/src/App.tsx
+++ mobile/src/App.tsx
@@ -1,11 +1,15 @@
 // Bibliotecas externas
-import React from 'react';
-import {View} from 'react-native';
+import React from "react";
+import { StatusBar, View } from "react-native";
 
 // Componentes
 
 const src: React.FC = () => {
-  return <View />;
+  return (
+  <>
+  <StatusBar barStyle='light-content' backgroundColor='#312e38'  />
+  <View style={{ backgroundColor: "#312e38", flex: 1 }} />
+  </>)
 };
 
 export default src;
