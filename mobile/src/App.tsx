3bcd71274a1de649d408bfeb72378b72e02e1afb
Igor Sasaki <igor@softable.com.br>
2021-05-17 22:22:03 -0300

    Iniciado a aplicação mobile com os arquivos iniciais

---
+++ mobile/src/App.tsx
@@ -0,0 +1,11 @@
+// Bibliotecas externas
+import React from 'react';
+import {View} from 'react-native';
+
+// Componentes
+
+const src: React.FC = () => {
+  return <View />;
+};
+
+export default src;
